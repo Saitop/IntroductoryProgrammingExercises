e748e385942b1c4cb9849c435ede9aa2c0a3bbf0
Hanxian Lin <saitoplam@hotmail.com>
2015-09-15 00:17:43 +0800

    add horizontal and vertical asterisks

--- src/com/exercises/Main.java
+++ src/com/exercises/Main.java
@@ -3,6 +3,8 @@ package com.exercises;
 public class Main {
 
     public static void main(String[] args) {
-        new TrianglePrinter().printTriangle();
+        new TrianglePrinter().printOneAsterisk();
+        new TrianglePrinter().printHorizontalAsterisksWithNumber(8);
+        new TrianglePrinter().printVerticalAsterisksWithNumber(3);
     }
 }
--- src/com/exercises/TrianglePrinter.java
+++ src/com/exercises/TrianglePrinter.java
@@ -4,7 +4,22 @@ package com.exercises;
  * Created by hxlin on 9/14/15.
  */
 public class TrianglePrinter {
-    public void printTriangle() {
+    public void printOneAsterisk() {
         System.out.println("*");
+        System.out.println("--------------------");
+    }
+
+    public void printHorizontalAsterisksWithNumber(int asterisk){
+        for (int i = 0; i < asterisk ; i++) {
+            System.out.print("* ");
+        }
+        System.out.println(" ");
+        System.out.println("--------------------");
+    }
+
+    public void printVerticalAsterisksWithNumber(int asterisk){
+        for (int i = 0; i < asterisk ; i++) {
+            System.out.println("*");
+        }
     }
 }

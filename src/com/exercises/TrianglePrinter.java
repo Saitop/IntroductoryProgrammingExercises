db2c90e72ce80206444fc1e38ab8484316b412b2
Hanxian Lin <saitoplam@hotmail.com>
2015-09-14 23:58:38 +0800

    print a * in command line

---
+++ src/com/exercises/TrianglePrinter.java
@@ -0,0 +1,10 @@
+package com.exercises;
+
+/**
+ * Created by hxlin on 9/14/15.
+ */
+public class TrianglePrinter {
+    public void printTriangle() {
+        System.out.println("*");
+    }
+}
--- tests/com/exercises/TriangleExerciseTests.java
+++
@@ -1,7 +0,0 @@
-package com.exercises;
-/**
- * Created by hxlin on 9/14/15.
- */
-public class TriangleExerciseTests {
-
-}
